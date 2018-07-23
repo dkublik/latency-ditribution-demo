8fa98894f819282cb96946dfbbfa4ccaa126209c
dkublik <dkublik@gmail.com>
2018-07-23 17:48:41 +0200

    initial

---
+++ src/main/java/pl/dk/micrometersimulation/MicrometerSimulationApp.java
@@ -0,0 +1,12 @@
+package pl.dk.micrometersimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrometerSimulationApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrometerSimulationApp.class, args);
+	}
+}
